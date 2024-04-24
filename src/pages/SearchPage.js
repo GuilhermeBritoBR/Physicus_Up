4a77ce23a019f31ed04c8af44a81dc4e21c64fc9
Nicolas Cardozo <125682448+Nicol4sC@users.noreply.github.com>
2024-04-24 13:35:42 -0300

    block_default_analytics

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { ScrollView, Text, TouchableOpacity, View } from "react-native";
 
 //importando styles Default Styles
@@ -5,10 +6,18 @@ import { DefaultStyles } from "../styles/DefaultStyles";
 
 //importando components
 import PesquisarComponent from "../components/TelaPesquisar/PesquisarComponent";
+=======
+import { ScrollView, Text, View } from "react-native";
+
+//importando styles Default Styles
+import { DefaultStyles } from "../styles/DefaultStyles";
+import PesquisarComponent from "../components/PesquisarPage/PesquisarComponent";
+>>>>>>> Stashed changes
 import HeaderComponent from "../components/HeaderComponent";
 import FooterComponent from "../components/FooterComponent";
 export default function SearchPage() {
   return (
+<<<<<<< Updated upstream
     <View style={[DefaultStyles.container]}>
       <HeaderComponent />
       <View style={[DefaultStyles.content]}>
@@ -16,6 +25,13 @@ export default function SearchPage() {
           <TouchableOpacity>
             <PesquisarComponent props_Text_Component={"Calcular ritmo"} />
           </TouchableOpacity>
+=======
+    <View style={DefaultStyles.container}>
+      <HeaderComponent />
+      <View style={DefaultStyles.content}>
+        <ScrollView>
+          <PesquisarComponent props_Text_Component={"Calcular ritmo"} />
+>>>>>>> Stashed changes
           <PesquisarComponent props_Text_Component={"Calcular IMC"} />
           <PesquisarComponent props_Text_Component={"Conferir Peso Ideal"} />
           <PesquisarComponent props_Text_Component={"Calcular ritmo"} />
