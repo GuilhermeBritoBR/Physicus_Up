d04470be46fd4ad53c7ace0b755e087011c79d33
Nicolas Cardozo <125682448+Nicol4sC@users.noreply.github.com>
2024-05-15 12:43:21 -0300

    analytics

--- src/pages/AnalyticsPage.js
+++ src/pages/AnalyticsPage.js
@@ -8,12 +8,24 @@ export default function AnalyticsPage() {
   return (
     <View style={DefaultStyles.container}>
       <HeaderComponent />
-      <View style={[DefaultStyles.content,{flex: 3}]}>
+      <View style={[DefaultStyles.content,{flex: 3, backgroundColor: 'rgba(0, 0, 0, 0.8)'}]}>
         <ScrollView>
+          <Text style={{color:'white', textAlign: 'center', fontSize: 20}}>Estatisticas</Text>
           <BlockDefaultComponent
-            titleCenter={"Desempenho"}
-            corDegrade1={"#000000"}
-            corDegrade2={"#000000"}
+            titleBlock={"Evolução de tempo"}
+            corDegrade1={"#B3B3B3"}
+            corDegrade2={"#B3B3B3"}
+          />
+          
+          <BlockDefaultComponent
+            titleBlock={"Evolução de peso"}
+            corDegrade1={"#B3B3B3"}
+            corDegrade2={"#B3B3B3"}
+          />
+          <BlockDefaultComponent
+            titleBlock={"Evolução de carga"}
+            corDegrade1={"#B3B3B3"}
+            corDegrade2={"#B3B3B3"}
           />
         </ScrollView>
       </View>
