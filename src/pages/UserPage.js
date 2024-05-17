2d11806acc5fb61ffa0e866efc95974f20ea014a
Davi Nogueira <davinogueirasenai@gmail.com>
2024-05-17 14:33:12 -0300

    UserpageOk

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -12,35 +12,56 @@ import UserPageContent from "../components/UserPageComponents/UserPageContent";
 
 export default function UserPage() {
   return (
-    
-      <LinearGradient
-        colors={["#000000", "#535353"]}
-        start={{ x: 0, y: 1 }}
-        end={{ x: 1, y: 0 }}
-        locations={[0, 1]}
-        style={[
-          {
-            flex: 3,
-            width: "100%",
-            borderWidth: 1,
-            borderColor: "black",
-            border: "solid",
-            borderRadius: 15,
-            backgroundColor: "#000000",
+
+    <View style={DefaultStyles.container}>
+
+    <LinearGradient
+      colors={["#000000", "#535353"]}
+      start={{ x: 0, y: 1 }}
+      end={{ x: 1, y: 0 }}
+      locations={[0, 1]}
+      style={[
+        {
+          flex: 3,
+          width: "100%",
+          borderWidth: 1,
+          borderColor: "black",
+          border: "solid",
+          backgroundColor: "#000000",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+      ]}
+    >
+      <View>
+        <Image
+          source={require("../assets/brito.jpg")}
+          style={{
+            width: 100,
+            height: 100,
+            borderRadius: 360,
+            marginTop: 200,
             justifyContent: "center",
             alignItems: "center",
-          },
-        ]}
-      >
-       
-        <View>
-          <Image
-            source={require("../assets/brito.jpg")} 
-            style={{width: 100, height: 100, borderRadius: 360, marginTop: 7,}}
-          />
-          <Text style={{color: "white", textAlign:"center" }}>Brito suricato</Text>
-        </View>
-        <UserPageContent/>  
-      </LinearGradient>
+          }}
+        />
+
+        <Text
+          style={{
+            color: "white",
+            textAlign: "center",
+            fontSize: 20,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          Guilherme
+        </Text>
+      </View>
+      
+      <UserPageContent />
+    </LinearGradient>
+    <FooterComponent />
+    </View>
   );
 }
