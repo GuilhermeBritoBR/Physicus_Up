9a3c0c0839219df4ae7e8f81b166dbf4769ff95c
Nicolas Cardozo <125682448+Nicol4sC@users.noreply.github.com>
2024-05-24 13:31:55 -0300

    update

--- src/pages/RunHome.js
+++ src/pages/RunHome.js
@@ -1,35 +1,58 @@
 import React from 'react';
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import { LinearGradient } from 'expo-linear-gradient';  
 import { DefaultStyles } from "../styles/DefaultStyles";
 import HeaderComponent from '../components/HeaderComponent';
 import FooterComponent from "../components/FooterComponent";
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 export default function RunHome() {
   return (
+
     <View style={DefaultStyles.container}>
+       <HeaderComponent />
       <LinearGradient
-        colors={["#1DB954", "#309F57"]}
-        start={{ x: 0, y: 0 }}
-        end={{ x: 1, y: 1 }}
-        style={{ flex: 1 }}
+        colors={["#000000", "#121212"]}
+        start={{ x: 1, y: 0 }}
+        end={{ x: 1, y: 0}}
+        style={[
+          ,{
+            flex: 3,
+            width: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+          },
+        ]}
       >
-        <HeaderComponent />
-        <View style={[DefaultStyles.content, { flex: 3 }]}>
-          <Text style={{ color: "white", textAlign: "center", fontSize: 28 }}>
+          <View style={{ flex: 1, marginTop: 20 }}>
+          <Text style={{ color: "white", textAlign: "center", fontSize: 35 }}>
             Corrida no final da tarde
           </Text>
           <Text style={{ color: "white", textAlign: "center", fontSize: 28 }}>
             10/05/2024
           </Text>
-          <View style={{ margin: 10 }}>
-            <Text style={{ color: "white", textAlign: "center", fontSize: 25 }}> 5 KM 20:00 </Text>
-            <Text style={{ color: "white", textAlign: "center", fontSize: 25 }}> 4’00 Min/Km </Text>
-            <Text style={{ color: "white", textAlign: "center", fontSize: 25 }}> Esforço: 9</Text>
+        
+          <View style={{ margin: 40, flex:2,}}>
+
+            <View style={{alignItems:"center"}}>
+            <Text style={{ color: "white", width: 250, textAlign:"center", fontSize: 28, padding: 10, borderColor:"white", borderBottomWidth: 2,}}>  Distância: 5 KM </Text>
+            <Text style={{ color: "white", width: 250, textAlign: "center", fontSize: 28, padding: 10, borderColor:"white", borderBottomWidth: 2, }}> Tempo: 20:00 </Text>
+            </View>
+
+            <View style={{alignItems:"center"}}>
+            <Text style={{ color: "white", width: 250, textAlign: "center", fontSize: 28, padding: 10, borderColor:"white", borderBottomWidth: 2,}}> Ritmo:4’00 Min/Km </Text>
+            <Text style={{ color: "white", width: 250, textAlign: "center", fontSize: 28, padding: 10, borderColor:"white", borderBottomWidth: 2, }}> Esforço: 9</Text>
+            </View>
+
+            
+
           </View>
-        </View>
-        <FooterComponent />
+          </View>
+          <MaterialCommunityIcons name="shoe-sneaker" size={250} color="#1DB954" />
       </LinearGradient>
-    </View>
+      <FooterComponent />
+
+      </View>
+
   );
 }
